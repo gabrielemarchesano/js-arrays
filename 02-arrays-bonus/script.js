44bb89728232095420980b62ebed0be0fb9ef936
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-08 17:23:18 +0200

    do bonus n.4 instruction

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -33,7 +33,14 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isFabioPresent;
+for(let i = 0; i < teachers.length; i++){
+  if(teachers.indexOf("Fabio") >= 0){
+    isFabioPresent = true;
+  }
+}
+console.log(`Fabio è presente? ${isFabioPresent}`);
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
