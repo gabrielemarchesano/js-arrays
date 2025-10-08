8de5dbb10d2ed36859d5fb4a5723007b2a89c40c
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-08 13:17:29 +0200

    do n.5 instruction

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -25,8 +25,10 @@ console.log(lastTeacher);
 const firstTeacher = teachers.shift();
 console.log(firstTeacher);
 
-
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa');
+console.log(teachers);
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
